f50429472642c69b704b7f3309486dd972e0d4e1
Pruthvi2340 <152501425+Pruthvi2340@users.noreply.github.com>
2024-01-20 13:12:47 +0530

    Update cert-manager-gce-ingress.sh

--- cert-manager/cert-manager-gce-ingress.sh
+++ cert-manager/cert-manager-gce-ingress.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+# Default class for nginx
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.k8s.io/v1
+kind: IngressClass
+metadata:
+  name: nginx-public
+  annotations:
+    ingressclass.kubernetes.io/is-default-class: "true"
+spec:
+  controller: k8s.io/ingress-nginx
+EOF
+
+# ////////// Nginx-controller Installation /////////////////#
+
+kubectl create clusterrolebinding cluster-admin-binding   --clusterrole cluster-admin   --user $(gcloud config get-value account)
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
+
+# Wait for the external IP to be assigned
+EXTERNAL_IP=""
+while [ -z "$EXTERNAL_IP" ]; do
+    EXTERNAL_IP=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    if [ -z "$EXTERNAL_IP" ]; then
+        echo "Waiting for external IP assignment..."
+        sleep 10
+    fi
+done
+
+echo "External IP has been assigned: $EXTERNAL_IP"
+
+# Apply Cloud DNS record set
+gcloud dns record-sets transaction start --zone=pruthvi-org
+gcloud dns record-sets transaction add --zone=pruthvi-org-zone --name=retail.pruthvi.org. --type=A --ttl=300 "$EXTERNAL_IP"
+gcloud dns record-sets transaction execute --zone=pruthvi-org
+
+
+# ////////// Cert-Manager Installation /////////////////#
+
 # Create namespace for Cert-Manager
 kubectl create namespace cert-manager
 
@@ -9,79 +47,153 @@ kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/
 # Wait for cert-manager components to be deployed
 kubectl wait --timeout=300s --for=condition=Available deployment --all -n cert-manager
 
-# Create a ClusterIssuer (change this based on your certificate provider)
+
+# staging certificate issuer in the the default namespace
 cat <<EOF | kubectl apply -f -
-# issuer-lets-encrypt-production.yaml
 apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
+kind: Issuer
 metadata:
-  name: letsencrypt-production
+  name: letsencrypt-staging
 spec:
   acme:
-    server: https://acme-v02.api.letsencrypt.org/directory
-    email: pruthvi990038@gmail.com # ❗ Replace this with your email address
+    # The ACME server URL
+    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    # Email address used for ACME registration
+    email: pruthvi990038@gmail.com
+    # Name of a secret used to store the ACME account private key
     privateKeySecretRef:
-      name: letsencrypt-production
+      name: letsencrypt-staging
+    # Enable the HTTP-01 challenge provider
     solvers:
-    - http01:
-        ingress:
-          name: web-ingress
+      - http01:
+          ingress:
+            ingressClassName: nginx
 EOF
 
-gcloud compute addresses create web-ip --global
-kubectl create ns hello-app
-kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0 -n hello-app
-kubectl expose deployment web --port=8080 -n hello-app
+# Prod certificate issuer in the the default namespace
+cat <<EOF | kubectl apply -f -
+apiVersion: cert-manager.io/v1
+kind: Issuer
+metadata:
+  name: letsencrypt-prod
+spec:
+  acme:
+    # The ACME server URL
+    server: https://acme-v02.api.letsencrypt.org/directory
+    # Email address used for ACME registration
+    email: pruthvi990038@gmail.com
+    # Name of a secret used to store the ACME account private key
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    # Enable the HTTP-01 challenge provider
+    solvers:
+      - http01:
+          ingress:
+            ingressClassName: nginx
+EOF
 
-# Create an example Ingress with a TLS certificate using Nginx Ingress class
+# Sample deployment
+
+cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: kuard
+spec:
+  selector:
+    matchLabels:
+      app: kuard
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: kuard
+    spec:
+      containers:
+      - image: gcr.io/kuar-demo/kuard-amd64:1
+        imagePullPolicy: Always
+        name: kuard
+        ports:
+        - containerPort: 8080
+EOF
+
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
-kind: Secret
+kind: Service
 metadata:
-  name: web-ssl
-  namespace: hello-app
-type: kubernetes.io/tls
-stringData:
-  tls.key: ""
-  tls.crt: ""
+  annotations:
+    cloud.google.com/neg: '{"ingress":true}'
+  name: kuard
+  namespace: default
+spec:
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 8080
+  selector:
+    app: kuard
+  type: ClusterIP
 EOF
 
-# Create an example Ingress with a TLS certificate using Nginx Ingress class
 cat <<EOF | kubectl apply -f -
-# ingress.yaml
 apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
-  name: web-ingress
-  namespace: hello-app
   annotations:
-    # This tells Google Cloud to create an External Load Balancer to realize this Ingress
-    kubernetes.io/ingress.class: gce
-    # This enables HTTP connections from Internet clients
-    kubernetes.io/ingress.allow-http: "true"
-    # This tells Google Cloud to associate the External Load Balancer with the static IP which we created earlier
-    kubernetes.io/ingress.global-static-ip-name: web-ip
+    cert-manager.io/issuer: letsencrypt-staging
+  name: kuard
+  namespace: default
 spec:
+  ingressClassName: nginx
+  rules:
+  - host: retail.pruthvi.org
+    http:
+      paths:
+      - backend:
+          service:
+            name: kuard
+            port:
+              number: 80
+        path: /
+        pathType: Prefix
   tls:
-    - secretName: web-ssl
-      hosts:
-        - cert-test.pruthvi.org
-  defaultBackend:
-    service:
-      name: web
-      port:
-        number: 8080
-  # This section is only required if TLS is to be enabled for the Ingress
-  tls:
-    - secretName: web-ssl
-      hosts:
-      - retail.pruthvi.org
+  - hosts:
+    - retail.pruthvi.org
+    secretName: tls-retail-pruthvi-org
 EOF
 
 # Monitor the Certificate resource for certificate issuance
 kubectl get pods -n cert-manager
-kubectl describe clusterissuers.cert-manager.io letsencrypt-production #letsencrypt-staging
-kubectl get certificate --all-namespaces
-kubectl describe ingress web-ingress -n hello-app
-kubectl get secret -n hello-app
-kubectl get certificates -n default --watch
+kubectl get certificate -n default
+kubectl get issuers -n default
+kubectl describe certificate tls-retail-pruthvi-org -n default
+kubectl describe secret tls-retail-pruthvi-org -n default
+kubectl describe ingress kuard -n default
+kubectl get secret -n default
+
+# Wait until the certificate is in the "True" state
+while [ "$(kubectl get certificate tls-retail-pruthvi-org -n default -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}')" != "True" ]; do
+    echo "Waiting for the certificate to be ready..."
+    sleep 10
+done
+
+echo "Certificate is ready. Proceeding with the overwrite annotation."
+
+# Apply overwrite annotation
+kubectl annotate ingress kuard cert-manager.io/issuer=letsencrypt-prod --overwrite -n default
+
+# Delete the secret (if needed)
+kubectl delete secret tls-retail-pruthvi-org -n default
+
+# Wait until the certificate is in the "True" state after deleting the secret
+while [ "$(kubectl get certificate tls-retail-pruthvi-org -n default -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}')" != "True" ]; do
+    echo "Waiting for the certificate to be ready after deleting the secret..."
+    sleep 10
+done
+
+# Beautified message
+echo "====================================="
+echo "  SSL Certificate Issued by Let's Encrypt"
+echo "====================================="
+echo "Ready for use!"
+echo
